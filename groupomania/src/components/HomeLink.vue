d90f3c684666def6578248512e9f61be18c2b4c3
Annaelle <annaelleemery512@gmail.com>
2021-12-07 12:29:17 +0100

    Vue installed

---
+++ groupomania/src/components/HomeLink.vue
@@ -0,0 +1,16 @@
+// ./components/HomeLink.vue
+<script>
+export default {
+	name: 'HomeLink'
+}
+</script>
+
+<template>
+	<a href="/"><i class="fas fa-home"></i></a> 
+</template>
+
+<style>
+	.fa-home {
+		color: red;
+	}
+</style>
